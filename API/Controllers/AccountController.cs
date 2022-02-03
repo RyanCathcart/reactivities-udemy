b47d758c0d016a3559f3922a74753ad219bf15e2
Ryan Cathcart <ryanwcathcart@gmail.com>
2022-02-03 01:26:00 -0500

    Removed Facebook Login button for Firefox compatibility

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,22 +1,12 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 using API.DTOs;
 using API.Services;
-using Domain;
 using Infrastructure.Email;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
 
 namespace API.Controllers
 {
@@ -27,22 +17,15 @@ namespace API.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly TokenService _tokenService;
-        private readonly IConfiguration _config;
-        private readonly HttpClient _httpClient;
         private readonly EmailSender _emailSender;
         public AccountController(UserManager<AppUser> userManager,
             SignInManager<AppUser> signInManager, TokenService tokenService,
-            IConfiguration config, EmailSender emailSender)
+            EmailSender emailSender)
         {
             _emailSender = emailSender;
-            _config = config;
             _tokenService = tokenService;
             _signInManager = signInManager;
             _userManager = userManager;
-            _httpClient = new HttpClient
-            {
-                BaseAddress = new System.Uri("https://graph.facebook.com")
-            };
         }
 
         [AllowAnonymous]
@@ -148,56 +131,6 @@ namespace API.Controllers
         {
             var user = await _userManager.Users.Include(p => p.Photos)
                 .FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
-            await SetRefreshToken(user);
-            return CreateUserObject(user);
-        }
-
-        [AllowAnonymous]
-        [HttpPost("fbLogin")]
-        public async Task<ActionResult<UserDto>> FacebookLogin(string accessToken)
-        {
-            var fbVerifyKeys = _config["Facebook:AppId"] + "|" + _config["Facebook:AppSecret"];
-
-            var verifyToken = await _httpClient
-                .GetAsync($"debug_token?input_token={accessToken}&access_token={fbVerifyKeys}");
-
-            if (!verifyToken.IsSuccessStatusCode) return Unauthorized();
-
-            var fbUrl = $"me?access_token={accessToken}&fields=name,email,picture.width(100).height(100)";
-
-            var response = await _httpClient.GetAsync(fbUrl);
-
-            if (!response.IsSuccessStatusCode) return Unauthorized();
-
-            var fbInfo = JsonConvert.DeserializeObject<dynamic>(await response.Content.ReadAsStringAsync());
-
-            var username = (string)fbInfo.id;
-
-            var user = await _userManager.Users.Include(p => p.Photos).FirstOrDefaultAsync(x => x.UserName == username);
-
-            if (user != null) return CreateUserObject(user);
-
-            user = new AppUser
-            {
-                DisplayName = (string)fbInfo.name,
-                Email = (string)fbInfo.email,
-                UserName = (string)fbInfo.id,
-                Photos = new List<Photo>
-                    {
-                        new Photo
-                        {
-                            Id = "fb_" + (string)fbInfo.id,
-                            Url = (string)fbInfo.picture.data.url,
-                            IsMain = true
-                    }}
-            };
-
-            user.EmailConfirmed = true;
-
-            var result = await _userManager.CreateAsync(user);
-
-            if (!result.Succeeded) return BadRequest("Problem creating user account");
-
             await SetRefreshToken(user);
             return CreateUserObject(user);
         }
--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -6,8 +6,6 @@ import { store } from "./store";
 
 export default class UserStore {
     user: User | null = null;
-    fbAccessToken: string | null = null;
-    fbLoading = false;
     refreshTokenTimeout: any;
 
     constructor() {
@@ -66,39 +64,6 @@ export default class UserStore {
 
     setDisplayName = (name: string) => {
         if (this.user) this.user.displayName = name;
-    }
-
-    getFacebookLoginStatus = async () => {
-        window.FB.getLoginStatus(response => {
-            if (response.status === "connected") {
-                this.fbAccessToken = response.authResponse.accessToken;
-            }
-        })
-    }
-
-    facebookLogin = () => {
-        this.fbLoading = true;
-        const apiLogin = (accessToken: string) => {
-            agent.Account.fbLogin(accessToken).then(user => {
-                store.commonStore.setToken(user.token);
-                this.startRefreshTokenTimer(user);
-                runInAction(() => {
-                    this.user = user;
-                    this.fbLoading = false;
-                });
-                history.push("/activities");
-            }).catch(error => {
-                console.log(error);
-                runInAction(() => this.fbLoading = false);
-            });
-        }
-        if (this.fbAccessToken) {
-            apiLogin(this.fbAccessToken);
-        } else {
-            window.FB.login(response => {
-                apiLogin(response.authResponse.accessToken);
-            }, {scope: "public_profile,email"});
-        }
     }
 
     refreshToken = async () => {
